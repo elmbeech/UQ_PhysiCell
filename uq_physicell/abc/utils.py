c991b36dc2ff1a97ef4fe23daa6a265c3c8a7a10
Heber Lima da Rocha <heberonly@gmail.com>
2026-03-20 16:57:32 -0400

    Add versioning information to metadata tables and functions

--- uq_physicell/abc/utils.py
+++ uq_physicell/abc/utils.py
@@ -1,4 +1,9 @@
 
+from abc_context.CalibrationContext import CalibrationContext
+from uq_physicell import __version__ as uq_physicell_version
+from pcdl import __version__ as pcdl_version
+from pyabc import __version__ as pyabc_version
+
 def insert_adaptive_weights_db(db_file, dict_distances, dict_adaptive_weights):
     import sqlite3
     conn = sqlite3.connect(db_file)
@@ -15,5 +20,35 @@ def insert_adaptive_weights_db(db_file, dict_distances, dict_adaptive_weights):
     for pop_id, weights in dict_adaptive_weights.items():
         placeholders = ', '.join(['?'] * (len(dict_distances)))
         cursor.execute(f"""INSERT OR IGNORE INTO AdaptiveDistance (Population, {', '.join(dict_distances.keys())}) VALUES (?, {placeholders})""", (pop_id, *weights))
+    conn.commit()
+    conn.close()
+
+def insert_metadata_db(db_file:str, abc_context:CalibrationContext):
+    import sqlite3
+    conn = sqlite3.connect(db_file)
+    cursor = conn.cursor()
+    # Create Metadata Table
+    cursor.execute(f"""CREATE TABLE IF NOT EXISTS Metadata (
+                    Method TEXT PRIMARY KEY,
+                    ObsData_Path TEXT,
+                    Ini_File_Path TEXT,
+                    StructureName TEXT,
+                    uq_physicell_version TEXT,
+                    pcdl_version TEXT,
+                    pyabc_version TEXT)""")
+    conn.commit()
+    conn.close()
+    # Insert the Data
+    conn = sqlite3.connect(db_file)
+    cursor = conn.cursor()
+    cursor.execute(f"""INSERT OR REPLACE INTO Metadata (Method, ObsData_Path, Ini_File_Path, StructureName, uq_physicell_version, pcdl_version, pyabc_version) VALUES (?, ?, ?, ?, ?, ?, ?)""", (
+        "ABC",
+        abc_context.obsData_path,
+        abc_context.model_config['ini_path'],
+        abc_context.model_config['struc_name'],
+        uq_physicell_version,
+        pcdl_version,
+        pyabc_version
+    ))
     conn.commit()
     conn.close()
